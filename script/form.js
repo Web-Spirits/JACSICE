5e29423f21f0336611684511c3d980e2f8812508
Web-Spirits <119612821+Web-Spirits@users.noreply.github.com>
2022-12-04 22:08:12 +0530

    Update form.js

--- script/form.js
+++ script/form.js
@@ -53,7 +53,7 @@ function pswd()
 	if (log_pswd==localStorage.getItem("ls_pswd")) 
 		{
 			alert("Login Success");
-			window.open("../JACSICE/html/jacsice.html")
+			window.open("../html/jacsice.html")
 		} else {
 			alert("Invalid Password");
 		}
